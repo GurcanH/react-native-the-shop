1662f02391255913f341d655ed97ec26c3786588
Gurcan Hamali <gurcanhamali@gmail.com>
2020-11-25 18:38:07 +1100

    167. Working on the Product Details Screen

--- screens/shop/ProductDetailsScreen.js
+++ screens/shop/ProductDetailsScreen.js
@@ -8,13 +8,20 @@ import {
   StyleSheet
 } from 'react-native';
 
+import Colors from '../../constants/Colors';
+
 const ProductDetailsScreen = props => {
   const selectedProduct = props.navigation.getParam('product');
 
   return (
-    <View>
-      <Text>{selectedProduct.title}</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
+      <View style={styles.actions}>
+        <Button color={Colors.primary} title='Add to Cart' onPress={() => {}} />
+      </View>
+      <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+      <Text style={styles.description}>{selectedProduct.description}</Text>
+    </ScrollView>
   );
 };
 
@@ -24,6 +31,26 @@ ProductDetailsScreen.navigationOptions = navData => {
   };
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 300
+  },
+  price: {
+    fontSize: 20,
+    color: '#888',
+    textAlign: 'center',
+    marginVertical: 20
+  },
+  description: {
+    fontSize: 14,
+    textAlign: 'center'
+  },
+  actions: {
+    marginVertical: 10,
+    alignItems: 'center',
+    marginHorizontal: 20
+  }
+});
 
 export default ProductDetailsScreen;
