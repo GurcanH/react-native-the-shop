2938ce4cddfbcfa4171267bb397fde412f73ee26
Gurcan Hamali <gurcanhamali@gmail.com>
2020-11-27 19:27:12 +1100

    203. Displaying a Loading Spinner & Handling

--- store/actions/products.js
+++ store/actions/products.js
@@ -7,27 +7,34 @@ export const SET_PRODUCTS = 'SET_PRODUCTS';
 
 export const fetchProducts = () => {
   return async dispatch => {
-    const response = fetch(
-      'https://react-native-shop-307f0.firebaseio.com/products.json'
-    );
-
-    const resData = await (await response).json();
-    const loadedProducts = [];
-
-    for (const key in resData) {
-      loadedProducts.push(
-        new Product(
-          key,
-          'u1',
-          resData[key].title,
-          resData[key].imageUrl,
-          resData[key].description,
-          resData[key].price
-        )
+    try {
+      const response = await fetch(
+        'https://react-native-shop-307f0.firebaseio.com/products.json'
       );
-    }
 
-    dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+      if (!response.ok) {
+        throw new Error('Something went wrong!!');
+      }
+      const resData = await (await response).json();
+      const loadedProducts = [];
+
+      for (const key in resData) {
+        loadedProducts.push(
+          new Product(
+            key,
+            'u1',
+            resData[key].title,
+            resData[key].imageUrl,
+            resData[key].description,
+            resData[key].price
+          )
+        );
+      }
+
+      dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+    } catch (err) {
+      throw err;
+    }
   };
 };
 
