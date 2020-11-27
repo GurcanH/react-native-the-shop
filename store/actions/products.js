bdb8ba2c316510864399468993f4a7b3e5f2c7b6
Gurcan Hamali <gurcanhamali@gmail.com>
2020-11-27 18:32:59 +1100

    201. Storing Products on a Server

--- store/actions/products.js
+++ store/actions/products.js
@@ -7,14 +7,34 @@ export const deleteProduct = productId => {
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
-  return {
-    type: CREATE_PRODUCT,
-    productData: {
-      title,
-      description,
-      imageUrl,
-      price
-    }
+  return async dispatch => {
+    const response = fetch(
+      'https://react-native-shop-307f0.firebaseio.com/products.json',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl,
+          price
+        })
+      }
+    );
+
+    const resData = await (await response).json();
+    console.log(resData);
+    dispatch({
+      type: CREATE_PRODUCT,
+      productData: {
+        title,
+        description,
+        imageUrl,
+        price
+      }
+    });
   };
 };
 
